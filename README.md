22e7b4d52ab1cc1d9067b10be5676ba4a134e7d5
zhangkunming <zhangkunming@xunlei.com>
2019-04-15 20:43:35 +0800

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+# qrcode
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+```
+
+## 二维码生成界面
+
+![qrcode](./src/assets/logo.png)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="qrcode">
+    <div>
+      <p>二维码大小</p>
+      <Input v-model="qrSize" type="textarea" :rows="1" placeholder="二维码大小,默认200" />
+    </div>
+    <div>
+      <p>二维码内容</p>
+      <Input v-model="content" type="textarea" :rows="5" placeholder="二维码内容" />
+    </div>
+    <div>
+      <!--colorDark="#f67b29"-->
+      <!--保存图片按钮-->
+      <vue-qr
+      :logoCornerRadius="0.1"
+      :logoSrc="imagePath"
+      :logoScale="0.2"
+      :text="content"
+      colorDark="#000"
+      colorLight="#fff"
+      :size="qrSize"
+      :margin="0"
+      :whiteMargin="true"
+      :dotScale="1"></vue-qr>
+      <p class="">二维码</p>
+      <!--保存图片按钮-->
+    </div>
+    <div>
+      <Button type="success" @click="downloadImg">保存图片</Button>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueQr from 'vue-qr'
+export default {
+  data () {
+    return {
+      content: '请输入二维码内容',
+      qrSize: '200',
+      imgName: '',
+      visible: false,
+      logoFile: '',
+      value: 'www.baidu.com',
+      imagePath: require('../assets/logo.png')
+    }
+  },
+
+  created () {},
+  components: {
+    VueQr
+  },
+
+  methods: {
+    downloadImg () {
+      var oQrcode = document.querySelector('#qrcode img')
+      var url = oQrcode.src
+      var a = document.createElement('a')
+      var event = new MouseEvent('click')
+      a.download = '二维码'
+      a.href = url
+      a.dispatchEvent(event)
+    }
+  }
+
+}
+</script>
